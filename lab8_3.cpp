9254108f015f15cec3ed1865ce243911278d0f2e
PhichaPrim <phichamon_duangmanee@cmu.ac.th>
2023-12-12 16:32:08 +0700

    lab8_3.cpp completed

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,37 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=0,h=0,b=0;
+    cout << "Enter your age: ";
+    cin >> a;
+    if(a<=25)
+    {
+        cout << "Enter your height: ";
+        cin >> h;
+        if(h<100){
+            cout << "Your character = Chopper";
+        }else if(h<180){
+            cout << "Your character = Usopp";
+        }else{
+            cout << "Enter your bounty: ";
+            cin >> b;
+            if(b<1100000001){
+                cout << "Your character = Sanji";
+            }else{
+                cout << "Your character = Zoro";
+            }
+        }
+    }else if(a>=25 && a<=60){
+        cout << "Enter your bounty: ";
+        cin >> b;
+        if(b>500000000){
+            cout << "Your character = Jinbe";
+        }else{
+            cout << "Your character = Franky";
+        }
+    }else{
+        cout << "Your character = Brook";
+    }
+}
